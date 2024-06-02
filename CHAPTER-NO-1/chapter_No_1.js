8870b9a5f618d955aec65f528511c5d37b0b3a53
Usman-52 <usman914.764@gmail.com>
2024-06-02 17:11:53 +0500

    i added some Question on Chapter no 13

--- CHAPTER-NO-1/chapter_No_1.js
+++ CHAPTER-NO-1/chapter_No_1.js
@@ -3,15 +3,15 @@
 //* alert is an pop up box which is giving message to the user.
 //*Example:
 
-alert("Asalam.o.Alikom");
+// alert("Asalam.o.Alikom");
 
 //* OR , alert is the short from which is mostly programmers proffers to use te short form but you can also use the full form which is shown in below statement.
 
-window.alert("W.Salam");
+// window.alert("W.Salam");
 
 //* addition
 
-alert(1+1);
+// alert(1+1);
 
 //* What is String;
 //* String is the javascript Data Type, Any massage which is Quoted in single or double Quotation mark ( inverted commas) is called String. 
@@ -19,15 +19,15 @@ alert(1+1);
 //* JavaScript ignoring spacing in syntax but don't ignoring spaces inside the String.
 //* For Example:
 
-alert("    Your're Precious");
+// alert("    Your're Precious");
 
 //* when you run the second Example then you'll see the clare space in Result in pop up message.
 
-alert("What's up              Charlie?");
+// alert("What's up              Charlie?");
 
 //* Example of the alert Message with single Quotation Mark ( inverted commas),
 
-alert('Alhamdulillah I am good , what about you usman?');
+// alert('Alhamdulillah I am good , what about you usman?');
 
 //! Note: 
 //* if you don't end statements with semicolon (;) it's ok but the good practice is to end up every statement with semicolon except those statements which
@@ -35,4 +35,3 @@ alert('Alhamdulillah I am good , what about you usman?');
 
 //* what is Statement in Javascript?
 //* Statement in JavaScript point of view will contains values, operators, keywords, expression etc .
-
