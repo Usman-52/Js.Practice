d0ccea630e8d6429871d885c610b85a4687ba883
Usman-52 <usman914.764@gmail.com>
2024-06-03 23:49:30 +0500

    i add some changes in the code

--- CHAPTER-NO-13/practice.js
+++ CHAPTER-NO-13/practice.js
@@ -7,21 +7,19 @@
 //* Instruction:
 //* Write  a program that takes three  numbers as input and determines which one is the largest.
 
-var firstNumber = +prompt("Enter your first number");
-var secondNumber = +prompt("Enter your Second number");
-var thirdNumber = +prompt("Enter your Third number");
+// var firstNumber = +prompt("Enter your first number");
+// var secondNumber = +prompt("Enter your Second number");
+// var thirdNumber = +prompt("Enter your Third number");
 
-if (firstNumber > secondNumber && firstNumber > thirdNumber){
-    console.log("The number " + firstNumber + " is the largest number among them.");
-}
-
-else if (secondNumber > firstNumber && secondNumber > thirdNumber){
-    console.log("The  number " + secondNumber + " is the largest number among them.");
-}
-
-else{
-    console.log("The number " + thirdNumber + " is the largest number among them.");
-}
+// if (firstNumber > secondNumber && firstNumber > thirdNumber){
+//     console.log(`The number ${firstNumber}  is the largest number among them.`);
+// }
+// else if (secondNumber > firstNumber && secondNumber > thirdNumber){
+//     console.log(`The  number  ${secondNumber} is the largest number among them.`);
+// }
+// else{
+//     console.log(`The number  ${thirdNumber} is the largest number among them.`);
+// }
 
 
 //! 2). Grade classification
@@ -33,27 +31,27 @@ else{
 // var yourMarks = +prompt("Enter your Marks");
 
 // if (yourMarks >= 90){
-//     console.log(yourName + ", Congratulation you got A+ grade.");
+//     console.log(`${yourName}, Congratulation you got A+ grade.`);
 // }
 
 // else if (yourMarks >= 80){
-//     console.log(yourName + ", Congrats you got A grade.");
+//     console.log(`${yourName},  Congrats you got A grade.`);
 // }
 
 // else if (yourMarks >= 70){
-//     console.log(yourName + ", You Got B+ Grade.");
+//     console.log(`${yourName}, You Got B+ Grade.`);
 // }
 
 // else if (yourMarks >= 60){
-//     console.log(yourName + ", You Got B Grade.");
+//     console.log(`${yourName}, You Got B Grade.`);
 // }
 
 // else if (yourMarks >= 50){
-//     console.log(yourName + ", You Got C Grade.");
+//     console.log(`${yourName}, You Got C Grade.`);
 // }
 
 // else{
-//     console.log(yourName + ", Sorry! Your are Fail.");
+//     console.log(`${yourName}, Sorry! Your are Fail.`);
 // }
 
 //! Even or Odd Number Identification
--- Class-Task/task.js
+++ Class-Task/task.js
@@ -1,18 +1,18 @@
-//! The weather program.
+//! Weather Forecasting Program.
 
 //* Instruction
 //* write a program when we have cloudy then it would be raining , it don't then it won't be raining.
 
 // var rain = prompt("Enter the Weather:");
 // if (rain === "cloudy"){
-//     console.log("take care of yourself");
+//     console.log("The rain would possibly start soon.");
 // }
 
 // else{
-//     console.log("it's ok wear a new clothes");
+//     console.log("There's No chances of Rain");
 // }
 
-//! BRT CARD BALANCE.
+//! BRT Card Ballance Program.
 //* INSTRUCTIONS
 //* Write the program how BRT card system works.
 
@@ -24,7 +24,7 @@
 //     console.log("sorry! you should recharge your ballance");
 // }
 
-//! BUY BURGER
+//! Burger Purchase Program.
 
 // * INSTRUCTIONS:
 //* Write a program for to buy burger if you have sufficient pocked money.
@@ -39,6 +39,77 @@
 //     console.log("Sorry! you don't have a sufficient pocket Mony.")
 // }
 
-//! 
+//! Even or Odd Numbers Program.
+//* INSTRUCTIONS:
+//* Write  Program To find the number you have entered is odd or even (using if).
 
-var number = +prompt("Enter the Number");
+// var enteredNumber = +prompt("Enter the Number");
+// if (enteredNumber % 2 === 0) {
+//     console.log(enteredNumber + " is Even Number");
+// }
+// else{
+//     console.log(enteredNumber + " is odd Number");
+// }
+
+//! 5). Fruit Shop Billing Program.
+//*INSTRUCTIONS:
+//* Write a program where you're buying 2 items but the items rate would be same , then create full bill of total items which you bought.
+
+// var orangePrice = +prompt("Enter Your first Item");
+// var bananaPrice = +prompt("Enter Your Second Item");
+// var totalPrice = orangePrice + bananaPrice;
+// console.log(`The total price of both:  ${totalPrice}`);
+
+//! 6). Traffic lights Response Program.
+
+//*INSTRUCTIONS:
+//* Write a traffic lights program where someone enter Red (stop),Yellow(get ready), Green(Go).
+
+// var trafficLight = prompt("Enter the color:");
+// if (trafficLight === "Red"){
+//     console.log("Stop 🔴");
+// }
+// else if (trafficLight === "Yellow"){
+//     console.log("Get Ready 🟡")
+// }
+// else if (trafficLight === "Green"){
+//     console.log("GO 🟢")
+// }
+// else{
+//     alert("Sorry! invalid color")
+// }
+
+//! Customer Discount program:
+
+//* INSTRUCTIONS:
+//* Make program that checks if a customer wants to buy more than 10 items.if they do, they get 20% discount, if they want to buy between 5 to 9 items, would getting 10% discount and if they want to buy less than 5 items, they would getting no discount.
+
+// var enteredItem = +prompt("How many items you wanna buy?");
+// var discount;
+// if (enteredItem >= 10){
+//     console.log(`Congratulation! you got ${discount = 20}% discount.`);
+// }
+
+// else if (enteredItem >= 5){
+//     console.log(`Congratulation! you got ${discount = 10}% discount. ` );
+// }
+// else{
+//     console.log("Sorry! You have No discount.");
+// }
+
+//! Bank balance withdrawal program:
+
+//* INSTRUCTIONS:
+//* Make a variable named bankBalance and store your bank balance in it, then make another variable for withdrawal. If the bank balance is greater than or equal to the withdrawal amount, the bank will give you the money; otherwise, a message will appear.
+
+// var bankBalance = 1000;
+// var enteredAmount = +prompt("Enter your Balance:");
+// var withdrawal = enteredAmount;
+
+// if (enteredAmount <= bankBalance){
+//     console.log(`You can withdrawal ${withdrawal}`);
+// }
+
+// else{
+//     console.log("Sorry! you don't have sufficient balance to withdrawal.");
+// }
