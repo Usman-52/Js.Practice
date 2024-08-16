1ace6027394db90c0566667bd1bd6fba03889982
Usman-52 <usman914.764@gmail.com>
2024-08-16 19:52:21 +0500

    i add some questions description to class tasks no. 35, 36

---
+++ CLASS-TASK-CH-NO-35-36/script.js
@@ -0,0 +1,35 @@
+//* Basic function 
+
+
+//* write a basic function called   `greetUser` that logs the massage "Hello , welcome to the class"
+
+function greatUser(){
+    console.log("Hello welcome to class");
+}
+
+greatUser();
+
+//* Function With Parameters 
+//* create a function named "DisplaysSquare" that takes a number is an argument and logs it's square in the console for example
+//* if you pass 5 as an argument the faction should lodged 25. 
+
+function displaySquare(num){
+    console.log(num * num);
+}
+
+displaySquare(5);
+
+//* Function with Multiple Parameter
+
+//* write a function called "sumOfTwoNumbers" that take two  numbers is parameters and logs their sum in the console. 
+//* for instance "sumOfTwoNUmbers"'(3.7)' should log 10.
+
+function sumOfTwoNumbers(num, num1){
+
+
+    sum = num + num1;
+    console.log(sum);
+}
+
+sumOfTwoNumbers(3, 7);
+
