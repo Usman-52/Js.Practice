ccacd95ee00c692aeb72f0cf4c50adb8b1e47c31
Usman-52 <usman914.764@gmail.com>
2024-08-17 23:53:48 +0500

    i add Class task of chapter NO. 37 and 38

---
+++ CLASS-TASK-CH-37-38/script.js
@@ -0,0 +1,59 @@
+//* P-NO.1
+
+
+// function calculate(num) {
+//     square = num * num;
+//     return square;
+// }
+
+// console.log(calculate(7));
+
+
+//* P-NO.2
+// var globalVar = 20;
+// function count() {
+//    var count = 3 * 2;
+//    return count;
+// }
+
+// console.log(globalVar); // 20 
+
+// console.log(count()); // 20
+
+// console.log(`Returned global Variable: ${globalVar} , Return Local variable ${count()} `)
+
+//* P-NO-3
+
+// var total = 50;
+
+// function addToTotal(num) {
+//      total = total + num; 
+//     return total;
+// }
+// console.log(addToTotal(10));
+
+//* P-NO.4
+
+// function greetUser(name) {
+//     return name;
+// }
+
+// console.log(`Hello ${greetUser("Alice")}`);
+
+
+//* P-NO.5
+
+
+// var status = "inactive";
+// function activate (status) {
+//     status = "Active";
+//     return status;
+// }
+
+// // console.log(activate())
+
+// console.log(`Global var value: ${status} & After changing: ${activate()}`)
+
+
+
+
