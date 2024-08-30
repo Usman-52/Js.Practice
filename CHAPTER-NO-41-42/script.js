a9e508220ae775f278abd7808ccbabca35365e26
Usman-52 <usman914.764@gmail.com>
2024-08-30 23:27:01 +0500

    I add new questions into chapter No.41 and 42

--- CHAPTER-NO-41-42/script.js
+++ CHAPTER-NO-41-42/script.js
@@ -20,3 +20,27 @@
 //     i++;
 // } while (i <= 2);
 
+
+//* Problem No.1
+//* I've coded an array. Code a do...while loop that looks for "pig" in the array. When it finds it, an alert displays saying, "Found it!" Use the length of the array as the loop limiter. Break out of the loop when "pig" is found.
+
+// var animals = ["horse", "ox", "cow", "pig", "duck"];
+// var i = 0;
+// do {
+//     if (animals[i] === "pig") {
+//         alert("Found it!");
+//     } 
+//     i++;
+// } while (i < animals.length);
+
+//* problem No.2
+//* Using a do...while loop, code an alert that loops 3 times, alerting the value of the counter each time.
+
+// var i = 0;
+// do {
+//     alert(i);
+//     i++;
+// } while (i <= 2);
+
+
+// * Problem No.5
