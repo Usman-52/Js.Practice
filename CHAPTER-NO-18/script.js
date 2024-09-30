b0eaebfeb49317d078e0512651497442513a948a
Usman-52 <usman914.764@gmail.com>
2024-09-30 21:27:53 +0500

    i add descriptive explanation to the for loop

--- CHAPTER-NO-18/script.js
+++ CHAPTER-NO-18/script.js
@@ -11,19 +11,62 @@
 //* write a program for the cleanest cities list of pakistan and the users would checking their own city weather their city belongs 
 //* to the cleanest cities of pakistan or not.
 
+// var cleanestCities = ["islamabad","abatabad", "peshawar", "murrey", "karak"];
+// var enterYourCity = prompt("Enter your city name:");
+
+// if (enterYourCity === cleanestCities[0]) {
+//     alert("belong to cleanest city");
+// }else if (enterYourCity === cleanestCities[1]) {
+//     alert(`${enterYourCity} is belong to the cleanest city of pakistan`);
+// }else if(enterYourCity === cleanestCities[2]) {
+//     alert(`${enterYourCity} is belong to the cleanest city of pakistan`);
+// }else if (enterYourCity === cleanestCities[3]) {
+//     alert(`${enterYourCity} is belong to the cleanest city of pakistan`);
+// }else if (enterYourCity === cleanestCities[4]) {
+//     alert(`${enterYourCity} is belong to the cleanest city of pakistan`);
+// }else{
+//     alert(`${enterYourCity} is NOT belong to the cleanest city of pakistan`);
+// }
+
+//?  What's the problem in the if Else statement?
+//* The main problem is that for every index, match , for every comparison we are writing code so it's become repetitive
+// * and we're giving for comparison , index, match a same massage (alert).
+
+//! so we can solve this problem with the help of For loop, that's why for loop came
+
+// ? What's loop?
+//* Repeatedly running or iterating something 
+
+//? syntax of for loop in JavaScript 
+
+//* for (Expression 1; Expression 2; Expression 3) {
+
+//* black of code to be executing
+
+// *}
+
+
+//? Expression 1:
+//* In Expression 1 we are creating a variable and assign value to that variable is executed (one time) before the execution of code black.
+//? Expression 2:
+//* defining the condition for the code block execution..
+//? Expression 3: 
+//* Expression 3 is executing (every time) after code block execution ( decrement or increment a variable which we creating in Expression 1)..
+
+//? Example:
+//* print numbers from 1 to 100
+
+// for (var num = 1; num <= 100; num++) {
+//     console.log(num);
+// }
+
+// ? Same above Example:
+
 var cleanestCities = ["islamabad","abatabad", "peshawar", "murrey", "karak"];
 var enterYourCity = prompt("Enter your city name:");
 
-if (enterYourCity = cleanestCities[0]) {
-    alert(` is belong to the cleanest cities of pakistan.`);
-}else if (enterYourCity = cleanestCities[1]) {
-    alert (` is belong to the cleanest cities of pakistan.`);
-}else if (enterYourCity = cleanestCities[2]) {
-    alert (` is belong to the cleanest cities of pakistan.`);
-}else if (enterYourCity = cleanestCities[3]) {
-    alert (` is belong to the cleanest cities of pakistan.`);
-}else if (enterYourCity = cleanestCities[4]) {
-    alert (` is belong to the cleanest cities of pakistan.`);
-}else {
-    alert(` is not belong to the cleanest cities of pakistan.`);
+for (var cityIndex = 0; cityIndex < cleanestCities.length; cityIndex++) {
+    if (enterYourCity === cleanestCities[cityIndex]) {
+        alert(`${enterYourCity} is in the cleanest cities of pakistan`)
+    }
 }
---
+++ CLASS-TASK-CH-65-66/script.js
@@ -0,0 +1,6 @@
+var renderImage = document.getElementById("srcInput");
+var imageSrc = document.getElementById("img-src").value;
+var changeWidth = document.getElementById("width-change").value;
+var changeHeight = document.getElementById("height-change").value;
+
+function 
