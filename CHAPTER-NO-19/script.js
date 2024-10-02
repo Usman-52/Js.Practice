eb376f58107dcb425715e03f297c3b9d369a6cda
Usman-52 <usman914.764@gmail.com>
2024-10-02 17:03:41 +0500

    i add some questions on for loop

--- CHAPTER-NO-19/script.js
+++ CHAPTER-NO-19/script.js
@@ -19,22 +19,22 @@
 
 // ? implementation
 
-var studentsRecord = ["Muhammad Usman", "Sadia Afridi", "Mohib Ullah", "Kalsoom", "Muhammad Huzaifa", "Abdul Haseeb"];
+// var studentsRecord = ["Muhammad Usman", "Sadia Afridi", "Mohib Ullah", "Kalsoom", "Muhammad Huzaifa", "Abdul Haseeb"];
 
 
-var meet = false; //* Flag variable 
-var searchStd = prompt("Enter Student Name:");
+// var meet = false; //* Flag variable 
+// var searchStd = prompt("Enter Student Name:");
 
-for (var stdIndex = 0; stdIndex < studentsRecord.length; stdIndex++) {
-    if (searchStd === studentsRecord[stdIndex]) {
+// for (var stdIndex = 0; stdIndex < studentsRecord.length; stdIndex++) {
+//     if (searchStd === studentsRecord[stdIndex]) {
         
-        meet = true;
-        break;  //* loopus interruptus
-    }
+//         meet = true;
+//         break;  //* loopus interruptus
+//     }
 
-    // console.log(studentsRecord[stdIndex])
+//     console.log(studentsRecord[stdIndex])
 
-}
+// }
 
 // if(meet) {
 //     alert(`Yes, ${searchStd} is enrolled with us..`)
@@ -67,3 +67,51 @@ for (var stdIndex = 0; stdIndex < studentsRecord.length; stdIndex++) {
 
 //? why we used here ?
 //* because whenever and wherever we meet the condition the program will terminating ...
+
+
+//!     SOME QUESTION ON FOR LOOP
+
+//? Question No.1
+//? How can a for loop be used to iterate over the elements of an array or list?
+//? Description:
+//*Think about how a for loop can access each individual element in a list or array, processing each element one by one.
+//* Consider what the loop variable represents during each iteration.
+
+// var stdNames = ["sadia", "usman", "khan", "inam", "muhammad", "iqbal"];
+
+// for (var i = 0; i < stdNames.length; i++) {
+//     console.log(stdNames[i]);
+// }
+
+//? Question No.2
+//? Sum of Odd Numbers
+//? Description:
+//* Given an array of integers, write a program that uses a for loop to calculate the sum of all odd numbers in the array.
+
+// var oddNumbers = [1,3,4,5,6,7,8,9,15,16,17,18]
+// sum = 0;
+// for (var i = 0; i < oddNumbers.length; i++) {  
+//     if(oddNumbers[i] % 2 === 1) {  //* you can also write if (oddNumbers[i] % 2 !== 0)
+//         sum += oddNumbers[i];
+//     } 
+// }
+
+// console.log(sum) //* 57
+
+//? Question No.3
+//? Count Vowels
+//? Description:
+//* Create a program that takes a string input and counts how many vowels (a, e, i, o, u) it contains using a for loop.
+
+var str = "Hello Usman, How are you?";
+var vowels = "aeiouAEIOU";
+var count = 0;
+
+
+for(var i = 0; i < str.length; i++ ) {
+    if(vowels.includes(str[i])) {
+        count++;
+    }
+}
+
+console.log(count)  //* 9
