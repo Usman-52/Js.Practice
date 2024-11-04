5a0cf6ee0f9c06dcfbc2e91044f023bc8ef56ef6
Usman-52 <usman914.764@gmail.com>
2024-11-04 22:43:15 +0500

    Data Type in JavaScript

---
+++ Code-&-Chai/01_DataTypes.js
@@ -0,0 +1,18 @@
+"use strict";  //* our all code will run as a newer version of js code.
+
+let score = 20 //* Number
+let myString = "Usman" //* String
+let greeting; //* String but undefined
+let myBoolean = true //*  Boolean it can be true or false
+let myNull = null //* type of null would be "object"
+let myGreeting = undefined //* type = undefined
+//* etc
+
+//* lets check the type of the Data variables which we declared
+
+// console.log(typeof score);
+// console.log(typeof myString);
+// console.log(typeof greeting);
+// console.log(typeof myBoolean);
+// console.log(typeof (myNull));
+console.log(typeof (myGreeting));
