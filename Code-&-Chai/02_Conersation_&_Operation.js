d531971ccb26ac669be0e9667a3b83c44cdea1ce
Usman-52 <usman914.764@gmail.com>
2024-11-04 23:28:38 +0500

    conversions

---
+++ Code-&-Chai/02_Conersation_&_Operation.js
@@ -0,0 +1,28 @@
+"use strict";
+// **--------------- Conversion --------------------- **//
+
+let myNumber = 20
+let numberInString = "22"
+let numberWithLetters = "23abc"
+let num = 1;
+
+//**---------------------- convert the variable ---------------**//
+
+let numberToString = String(myNumber)
+// console.log(numberToString);
+// console.log(typeof numberToString);
+
+let stringToNumber = Number(numberInString)
+// console.log(stringToNumber);
+// console.log(typeof stringToNumber);
+
+let numbersAndLetters = Number(numberWithLetters)
+// console.log(numbersAndLetters);
+// console.log(typeof numbersAndLetters); //* here the type of this variable is number 
+//* the value is NaN (Not a Number) because whenever we convert number in string with 
+//* Alphabets or symbols etc will come NaN cause of it's not pure number
+
+let numToBoolean = Boolean(num)
+// console.log(numToBoolean); //* output will come true cause 1 = true and 0 = false
+// console.log(typeof numToBoolean); //* the Type will be boolean
+
