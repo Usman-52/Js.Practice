71b86f0649a98d9ac982d594442bf0a18f718e70
Usman-52 <usman914.764@gmail.com>
2025-02-24 23:41:27 +0500

    add Frist Challange Code

--- Jhons-JavaScrip-Course-Practice/Part-1/J_practice.js
+++ Jhons-JavaScrip-Course-Practice/Part-1/J_practice.js
@@ -99,3 +99,33 @@
 // }
 
 // console.log(numberOfPotato(1));
+
+//?  Coding Challenge #1 
+//* Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: 
+//* BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter). 
+//* Your tasks: 
+//* 1. Store Mark's and John's mass and height in variables 
+//* 2. Calculate both their BMIs using the formula (you can even implement both versions) 
+//* 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John. 
+//* Test data: 
+//* Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall. 
+//* Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+
+//* Data No.1
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 95;
+// const johnHeight = 1.95;
+
+//* Data No.2
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
+
+let markBMI = markMass/(markHeight ** 2);
+let johnBMI = johnMass / (johnHeight * johnHeight);
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(`Mark BMI is: ${markBMI} & John BMI is: ${johnBMI} so Mark BMI is greater Then John: ${markHigherBMI}`);
