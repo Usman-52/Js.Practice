824012b246080a25bb22a6da593566c2813c1cfb
Usman-52 <usman914.764@gmail.com>
2025-02-24 23:53:54 +0500

    add Coding Challenge No.2

--- Jhons-JavaScrip-Course-Practice/Part-1/J_practice.js
+++ Jhons-JavaScrip-Course-Practice/Part-1/J_practice.js
@@ -128,4 +128,17 @@ let markBMI = markMass/(markHeight ** 2);
 let johnBMI = johnMass / (johnHeight * johnHeight);
 let markHigherBMI = markBMI > johnBMI;
 
-console.log(`Mark BMI is: ${markBMI} & John BMI is: ${johnBMI} so Mark BMI is greater Then John: ${markHigherBMI}`);
+// console.log(`Mark BMI is: ${markBMI} & John BMI is: ${johnBMI} so Mark BMI is greater Then John: ${markHigherBMI}`);
+
+//? Coding Challenge #2 
+//* Use the BMI example from Challenge #1, and the code you already wrote, and improve it. 
+//* Your tasks: 
+//* 1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!" 
+//* 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!" 
+
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI : ${markBMI.toFixed(2)} is higher than ${johnBMI.toFixed(2)} `)
+}else {
+    console.log(`John's BMI : ${johnBMI.toFixed(2)} is higher than ${markBMI.toFixed(2)} `)
+}
