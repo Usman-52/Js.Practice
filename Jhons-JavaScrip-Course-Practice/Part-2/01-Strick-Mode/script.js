37c4e77a7379ce925bb44bb3c340475d3c2e9c88
Usman-52 <usman914.764@gmail.com>
2025-04-22 12:22:47 +0500

    add Practice on Function in Javascript

--- Jhons-JavaScrip-Course-Practice/Part-2/01-Strick-Mode/script.js
+++ Jhons-JavaScrip-Course-Practice/Part-2/01-Strick-Mode/script.js
@@ -14,8 +14,6 @@
 // // // alert(`You have entered ${lengthOfYourMassage} and you have remaining ${280 - lengthOfYourMassage}`)
 // alert(lengthOfYourMassage)
 
-
-
 // var massage = prompt("Enter your Name")
 // var sliced = massage.slice(0, 1)
 // sliced = sliced.toUpperCase()
@@ -24,5 +22,59 @@
 
 // alert(sliced)
 
+//! FUNCTIONS IN JAVASCRIPT
+//? WHAT IS FUNCTION?
+//* Function is the chunk (Block) of code which we can reuse again and again without repeating our code.
+//* A function can be with or without parameters, it also can return value and can't be.
+
+//? This is called function declaration, here we simply declaring the functions
+
+//? For Example:
+//* A function without parameters and return something just log something to the console.
+
+// function logger() {
+//     console.log("Hello Khan!")
+// }
+
+//* Calling, Invoking, Executing the function
+// logger()
+
+//? A Function with parameters and returning something
+
+// function juiceMachine(Apple, Banana) {
+//   const juice = `${Apple} Apple and ${Banana} Banana juice`;
+//   return juice;
+// }
+
+//* if u need to reuse your function then we can save the retuning value of the function in variable
+
+// const appleJuice = juiceMachine(5, 0);
+// console.log(appleJuice);
+
+//? OR
+
+// console.log(juiceMachine(2, 4));
 
 
+//? Function Declaration vs Function Expressions
+
+//? Function Declaration:
+
+function clcAge1(birthYear, currentDate) {
+    return currentDate - birthYear
+}
+
+const age1 = clcAge1(2000, 2025)
+// console.log(age1)
+
+//? Function Expressions
+//* Expression Functions giving us a value which can store in a variable.
+//* EXpression functions we can't call them before defining the function.
+
+
+const clcAge2 = function() {
+    return currentDate - birthYear
+}
+
+const age2 = clcAge2(2000, 2025)
+console.log(age1, age2)
